77887465dc2aab5e1f8991e3d1dc9df5061e49c7
hemavmane <hemamane5376@gmail.com>
2022-09-25 13:29:43 +0530

    todo=step1

--- todo-list/src/Compnent/Todo.js
+++ todo-list/src/Compnent/Todo.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Todo() {
+  const [task, setTask] = useState([]);
+ const [items, setItems]=useState("");
+  const handleadd = (e) => {
+    setTask([...task,items])
+    setItems("")
+  };
+
+  const handledelete = () => {};
+
+  const handleedit = () => {};
   return (
-    <div>Todo</div>
-  )
+    <>
+      <input  type="text" onChange={(e)=>setItems(e.target.value)} defaultValue={task} />
+      <button onClick={handleadd}>Add task</button>
+      {task.map(tasks => (
+        //  console.log(task)
+        <div key={tasks}>
+          <input  type="text" defaultValue={tasks} />
+          <button onClick={handledelete}>Delete Task</button>
+          <button onClick={handleedit}>Edit Task</button>
+        </div>
+      ))} 
+    </>
+  );
 }
